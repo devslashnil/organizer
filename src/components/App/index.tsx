9a23d2f0ed1daf5fdc47d2c2ebc19e7a345a637a
Mikhail Potapov <devslashnil@gmail.com>
2021-01-12 19:19:11 +0300

    0.0.1

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Input } from '@material-ui/core';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Input />
+      ssssss
+    </div>
+  );
+};
+
+export default App;
